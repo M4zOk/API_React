40320cadb2a2f7003e421a753b43109e06e83401
M4zOk <maz-ign@yandex.ru>
2022-11-07 16:01:02 +0700

    API react

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
 import React, {FormEvent, useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+type Post = {
+    id: string;
+    userID: string;
+    body: string;
+    title: string;
+}
 
+type Post2={
+    name: string;
+    email: string;
+
+}
 
 function App() {
+
   const [id, setId] = useState("");
+  const [post, setPost]= useState<Post | undefined> ();
+  const [post2, setPost2]= useState<Post2 | undefined> ();
 
   const onSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
-    const data = await response.json()
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    const data = await response.json();
+    setPost(data);
+
+    const response2 = await fetch(`https://jsonplaceholder.typicode.com/users/${data.userId}`);
+    const data2 = await response2.json();
+    setPost(data);
+      setPost2(data2);
     console.log(data);
+
   };
 
 
@@ -22,9 +42,16 @@ function App() {
         ID:<input value={id} onChange={e => setId(e.target.value)}/>
       </h1>
 
-      <button>Получить данные!</button>
-      </form>
+      <button>Получить данные!</button><br/>
 
+      <b>{post?.title}</b><br/>
+       {post?.body}
+       <br/>
+       <br/>
+        <b>{post2?.name}</b><br/>
+          {post2?.email}
+      </form>
+    </div>
   );
 }
 
